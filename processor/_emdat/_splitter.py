0b7cd12fdd9c43fdd34e4453c93b82e2c98a3738
Dekun Zhang <91729508+DekunZhang@users.noreply.github.com>
2023-03-09 13:53:10 +0000

    docs: add documentation for emdat splitter

--- processor/_emdat/_splitter.py
+++ processor/_emdat/_splitter.py
@@ -4,12 +4,31 @@ __all__ = ['EMDATSplitter']
 
 
 class EMDATSplitter:
+    """A class for splitting a pandas DataFrame into a dictionary of DataFrames
+    based on the country and the disaster type.
+
+    Attributes:
+        __data (dict): A dictionary of DataFrames where the keys are country
+            names and the values are dictionaries of DataFrames where the keys
+            are disaster types and the values are DataFrames containing the data
+            for that country and disaster type.
+        __df (pd.DataFrame): The DataFrame to be split.
+
+    Methods:
+        data(): Returns the __data attribute.
+    """
     def __init__(self, df: pd.DataFrame):
+        """Initializes a new instance of the EMDATSplitter class.
+
+        Args:
+            df (pd.DataFrame): The DataFrame to be split.
+        """
         self.__data = {}
         self.__df = df
         self.__start()
 
     def __start(self):
+        """Splits the DataFrame based on the country name."""
         countries = self.__df['Country'].unique()
         for country in countries:
             self.__data[country] = self.__df[self.__df['Country'] == country]
@@ -17,6 +36,7 @@ class EMDATSplitter:
         self.__split()
 
     def __split(self):
+        """Splits the DataFrames for each country based on the disaster type."""
         for country, df in self.__data.items():
             required_events = ['Storm', 'Flood', 'Earthquake']
             events = {
@@ -27,4 +47,5 @@ class EMDATSplitter:
 
     @property
     def data(self):
+        """Returns the __data attribute."""
         return self.__data
