8b681c01d6693e62050d059a2053ca308d0f7701
Dekun Zhang <91729508+DekunZhang@users.noreply.github.com>
2023-03-09 14:34:04 +0000

    docs: add documentation for subtype file getter

--- processor/_file_getters/_subtype_file_getter.py
+++ processor/_file_getters/_subtype_file_getter.py
@@ -4,6 +4,15 @@ __all__ = ["SubtypeFileGetter"]
 
 
 class SubtypeFileGetter:
+    """A class to retrieve subtype files and store them as a dictionary.
+
+    Attributes:
+        _SUBTYPE_FOLDER_NAME (str): Name of the subtype folder.
+
+    Args:
+        data_folder (Directory): A `Directory` object containing the subtype
+            files.
+    """
     _SUBTYPE_FOLDER_NAME = "categorizations"
 
     def __init__(self, data_folder: Directory):
@@ -27,5 +36,12 @@ class SubtypeFileGetter:
 
     @property
     def subtypes(self):
+        """
+        Returns the subtype files as a dictionary, with subtype names as keys
+        and `File` objects as values.
+
+        Returns:
+            dict: A dictionary of subtype files.
+        """
         return self.__subtypes
 
