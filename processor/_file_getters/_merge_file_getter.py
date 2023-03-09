89eeb73b6039fe29d61e3053040724049cdee8c0
Dekun Zhang <91729508+DekunZhang@users.noreply.github.com>
2023-03-09 14:22:52 +0000

    docs: add documentation for merge file getter

--- processor/_file_getters/_merge_file_getter.py
+++ processor/_file_getters/_merge_file_getter.py
@@ -4,10 +4,43 @@ __all__ = ["MergeFileGetter"]
 
 
 class MergeFileGetter:
+    """
+    A class that retrieves countries and output directories for merging files.
+
+    Attributes:
+        _RECORD_FOLDER_NAME (str): A private attribute representing the folder
+            name where the record files are stored.
+        _EVENT_FOLDER_NAME (str): A private attribute representing the folder
+            name where the event files are stored.
+        __record_folder (Directory): A private attribute representing the
+            directory object where the record files are located.
+        __output_folder (Directory): A private attribute representing the
+            directory object where the event files will be stored.
+        __countries (list): A private attribute representing the list of
+            countries where the record files are located.
+
+    Args:
+        data_folder (Directory): A Directory object representing the directory
+            where the record files are located.
+
+    Properties:
+        countries (list): A property representing the list of countries where
+            the record files are located.
+        output_folder (Directory): A property representing the directory where
+            the event files will be stored.
+
+    """
     _RECORD_FOLDER_NAME = "records"
     _EVENT_FOLDER_NAME = "events"
 
     def __init__(self, data_folder: Directory):
+        """
+        Constructor of the MergeFileGetter class.
+
+        Args:
+            data_folder (Directory): A Directory object representing the
+                directory where the record files are located.
+        """
         self.__record_folder = \
             data_folder.find_directory(
                 MergeFileGetter._RECORD_FOLDER_NAME
@@ -23,8 +56,22 @@ class MergeFileGetter:
 
     @property
     def countries(self):
+        """
+        A property representing the list of countries where the record files
+        are located.
+
+        Returns:
+            list: The list of countries where the record files are located.
+        """
         return self.__countries
 
     @property
     def output_folder(self):
+        """
+        A property representing the directory where the event files will be stored.
+
+        Returns:
+            Directory: A Directory object representing the directory where the
+                event files will be stored.
+        """
         return self.__output_folder
